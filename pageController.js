e8b3709bc6bf14fb16abe860f12f79bcf92dc84a
Mint1832 <mint18032@gmail.com>
2022-07-23 18:23:23 +0700

    Complete the program.

--- pageController.js
+++ pageController.js
@@ -1,10 +1,33 @@
 const pageScraper = require('./pageScraper');
+const XLSX = require('xlsx');
+
 async function scrapeAll(browserInstance) {
 	let browser;
 	try {
 		browser = await browserInstance;
-		await pageScraper.scraper(browser);
+		let scrapedData = [];
+		scrapedData = await pageScraper.scraper(browser);
+		await browser.close();
+		console.log(scrapedData);
 
+		try {
+			const file = XLSX.readFile('./result.xlsx');
+			const ws = XLSX.utils.json_to_sheet(scrapedData);
+			// Adjust collum width.
+			let wscols = [
+				{ wpx: 1190 },
+				{ wpx: 90 },
+				{ wpx: 60 },
+				{ wpx: 250 }
+			];
+			ws['!cols'] = wscols;
+			XLSX.utils.book_append_sheet(file, ws, "Scraped Data");
+			// Writing to file
+			XLSX.writeFile(file, './result.xlsx');
+			console.log("The data has been scraped and saved successfully! View it at './result.xlsx'");
+		} catch (error) {
+			console.log("Could not save data to file: ", error);
+		}
 	}
 	catch (err) {
 		console.log("Could not resolve the browser instance => ", err);
--- pageScraper.js
+++ pageScraper.js
@@ -14,25 +14,55 @@ const scraperObject = {
 			return links;
 		});
 
-		// Loop through each of those links, open a new page instance and get the relevant data from them
-		let pagePromise = (link) => new Promise(async (resolve, reject) => {
-			let articleUrls;
-			let newPage = await browser.newPage();
-			await newPage.goto(link);
-			articleUrls = await newPage.$$eval('#main-content .media-body', articleLinks => {
-				articleLinks = articleLinks.map(el => el.querySelector('.col-md-10 > a').href)
-				return articleLinks;
-			});
-			resolve(articleUrls);
-			await newPage.close();
-		});
+		async function articleScraper() {
+			let scrapedData = [];
+			// Get articles' urls 
+			async function findUrls(link) {
+				let articleUrls;
+				await page.goto(link);
+				articleUrls = await page.$$eval('#main-content .media-body', articleLinks => {
+					articleLinks = articleLinks.map(el => el.querySelector('.col-md-10 > a').href)
+					return articleLinks;
+				});
+				return articleUrls;
+			}
 
-		for (link in urls) {
-			let currentPageData = await pagePromise(urls[link]);
-			// scrapedData.push(currentPageData);
-			console.log(currentPageData);
+			// Get data from each article
+			let articlePromise = (link) => new Promise(async (resolve, reject) => {
+				let pageData = {};
+				let newPage = await browser.newPage();
+				await newPage.goto(link);
+				pageData['Tên bài báo'] = await newPage.$eval('header > h2', text => text.textContent.replace(/\s+/g, ' ').trim());
+				try {
+					pageData['Ngày đăng'] = await newPage.$eval('.list-group > .date-published', text => text.textContent.replace(/\s+/g, ' ').replace('Đã đăng: ', '').trim());
+				} catch (error) {
+					pageData['Ngày đăng'] = ''; // Some articles lack of this data.
+				}
+				pageData['Số báo'] = await newPage.$eval('.panel-body > .title', text => text.textContent.replace(/\s+/g, ' ').trim());
+				try {
+					pageData['Địa chỉ DOI'] = await newPage.$eval('.list-group > .doi > a', text => text.textContent.replace(/\s+/g, ' ').trim());
+				} catch (e) {
+					pageData['Địa chỉ DOI'] = '';
+				}
+				resolve(pageData);
+				scrapedData.push(pageData);
+				await newPage.close();
+			});
+
+			for (link in urls) {
+				let articleUrls = await findUrls(urls[link]);
+				for (articleLink in articleUrls) {
+					await articlePromise(articleUrls[articleLink]);
+				}
+			}
+
+			return scrapedData;
 		}
 
+		let data = await articleScraper();
+		await page.close();
+		// console.log(data);
+		return data;
 
 	}
 }
